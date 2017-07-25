8ecf831fa32233ba8fd38f2145d201ca36b724da
akash <akashkarki10@gmail.com>
2017-07-25 22:28:46 +0530

    basic layout done

---
+++ files/JS/main.js
@@ -0,0 +1,16 @@
+var index = 0;
+var show="image1";
+carousel();
+function carousel(){
+        $('.slide').removeClass(show);//chaining
+        $(".slide").addClass("hidden");
+        index++;
+        if (index > 2) {
+        index = 1;
+    }
+    show="image"+index; 
+    console.log(show); 
+    $('.slide').removeClass("hidden");//chaining
+    $(".slide").addClass(show);
+    setTimeout(carousel, 10000);  
+}
