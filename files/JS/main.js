dd4d2cd2f2484a6de8fe2547ad2f39aac4958594
akash <akashkarki10@gmail.com>
2017-07-29 23:42:53 +0530

    clarifai api not working

--- files/JS/main.js
+++ files/JS/main.js
@@ -1,19 +1,3 @@
-var index = 0;
-var show="image1";
-carousel();
-function carousel(){
-        $('.slide').removeClass(show);//chaining
-        $(".slide").addClass("hidden");
-        index++;
-        if (index > 2) {
-        index = 1;
-    }
-    show="image"+index;
-    console.log(show);
-    $('.slide').removeClass("hidden");//chaining
-    $(".slide").addClass(show);
-    setTimeout(carousel, 10000);
-}
 
 
 var foodie = angular.module('foodie',['ngRoute']);
@@ -37,6 +21,22 @@ foodie.config(function ($routeProvider) {
 
 
 foodie.controller('RestrauntController',function($scope) {
+  var index = 0;
+  var show="image1";
+  carousel();
+  function carousel(){
+          $('.slide').removeClass(show);//chaining
+          $(".slide").addClass("hidden");
+          index++;
+          if (index > 2) {
+          index = 1;
+      }
+      show="image"+index;
+      console.log(show);
+      $('.slide').removeClass("hidden");//chaining
+      $(".slide").addClass(show);
+      setTimeout(carousel, 10000);
+  }
 
   $scope.restaurants = [{
   	name: 'Farzi Cafe',
@@ -90,7 +90,7 @@ foodie.controller('loginController',function($scope,$location) {
 
 
 
-foodie.controller('showRestraunt',function($scope,$routeParams) {
+foodie.controller('showRestraunt',function($scope,$routeParams,$http) {
   console.log($routeParams.id);
 var restraunt=[{
   name: 'Farzi Cafe',
@@ -103,7 +103,11 @@ var restraunt=[{
   cost: '2200',
   hours: '12 Noon to 1 AM (Mon-Sun)',
   image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg',
-  backimage:'https://b.zmtcdn.com/data/res_imagery/18233593_RESTAURANT_00272acd0235af309a0bbe4843e53499.jpg?output-format=webp'
+  backimage:'https://b.zmtcdn.com/data/res_imagery/18233593_RESTAURANT_00272acd0235af309a0bbe4843e53499.jpg?output-format=webp',
+  bestDish: {
+	name: 'cheese dip cookie',
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg?fit=around%7C800%3A792&crop=800%3A792%3B%2A%2C%2A'
+},
 },
 {
   name: 'Brown Sugar',
@@ -116,7 +120,11 @@ var restraunt=[{
   cost: '600',
   hours: '12 Noon to 1 AM (Mon-Sun)',
   image: 'https://b.zmtcdn.com/data/pictures/chains/5/1395/b9904d3c19534d029ca3a3f0643b2849_featured_v2.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
-  backimage:"https://b.zmtcdn.com/data/res_imagery/1395_CHAIN_d65b48669fa4bfcda9bb2a8c49413be2.jpg?output-format=webp"
+  backimage:"https://b.zmtcdn.com/data/res_imagery/1395_CHAIN_d65b48669fa4bfcda9bb2a8c49413be2.jpg?output-format=webp",
+  bestDish: {
+	name: 'Brown Rolls',
+	image: 'https://b.zmtcdn.com/data/reviews_photos/5d4/6833bccf5e509ae1bd8b3f1e9c4145d4_1499413777.jpg?fit=around%7C800%3A1200&crop=800%3A1200%3B%2A%2C%'
+},
 },
 {
   name: 'Whipped',
@@ -129,8 +137,37 @@ var restraunt=[{
   cost: '500',
   hours: '12 Noon to 1 AM (Mon-Sun)',
   image: 'https://b.zmtcdn.com/data/pictures/9/2649/0661277daa17c54c8d2bdcf336c37117_featured_v2.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A',
-  backimage:"https://b.zmtcdn.com/data/res_imagery/2649_RESTAURANT_7576f3e8622589e1dd3ef23896c305f3.jpg?output-format=webp"
+  backimage:"https://b.zmtcdn.com/data/res_imagery/2649_RESTAURANT_7576f3e8622589e1dd3ef23896c305f3.jpg?output-format=webp",
+  bestDish: {
+	name: 'chocolate cake',
+	image: 'https://b.zmtcdn.com/data/pictures/chains/9/2649/e202ef113e6cad530754f2f42737e86b.jpg?fit=around%7C600%3A386&crop=600%3A386%3B%2A%2C%2A'
+},
 },
 ];
 $scope.restraunt=restraunt[$routeParams.id-1];
+
+$scope.getIngredients = function(url) {
+	var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
+	$http({
+		'method': 'POST',
+		'url': 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs',
+		'headers': {
+			'Authorization': 'b7f174c5e0e042979e576bd2ce176d59',
+			'Content-Type': 'application/json'
+		},
+		'data': data
+	}).then(function (response) {
+			var ingredients = response.data.outputs[0].data.concepts;
+  			var list = '';
+  			for (var i =0;i < ingredients.length;i++) {
+  				list += '<div class="ingredient">' + ingredients[i].name + '</div>'
+  			}
+    		// $('.ingredients').html(list);
+    		console.log(list);
+        }, function (xhr) {
+        	console.log(xhr);
+        })
+	}
+
+
 })
